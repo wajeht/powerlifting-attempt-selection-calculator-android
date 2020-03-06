dec6aa5a6d198c70632238caec351a4169f35cbb
wajeht <zombyard@gmail.com>
2020-03-06 01:41:53 -0600

    - all calculations works now - added frist, second, third attemp mthods

--- app/src/main/java/com/jawstrength/powerliftingattemptselectioncalculator/MainActivity.java
+++ app/src/main/java/com/jawstrength/powerliftingattemptselectioncalculator/MainActivity.java
@@ -8,10 +8,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
-import javax.xml.transform.Result;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,30 +35,71 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                //Toast.makeText(getApplicationContext(),"Hello toast",Toast.LENGTH_SHORT).show();
-
                 int sqRepInput = Integer.parseInt(sqRepID.getText().toString());
                 int sqWeightInput = Integer.parseInt(sqWeightID.getText().toString());
                 int sqRpeInput = Integer.parseInt(sqRpeID.getText().toString());
+                int sq1RM = oneRepMax(sqRepInput,sqWeightInput, sqRpeInput);
+                int sq1st = firstAttempt(sq1RM);
+                int sq2nd = secondAttempt(sq1RM);
+                int sq3rd = thirdAttempt(sq1RM);
 
-                int sq1RM = calculate1RM(sqRepInput,sqWeightInput, sqRpeInput);
+                int bnRepInput = Integer.parseInt(bnRepID.getText().toString());
+                int bnWeightInput = Integer.parseInt(bnWeightID.getText().toString());
+                int bnRpeInput = Integer.parseInt(bnRpeID.getText().toString());
+                int bn1RM = oneRepMax(bnRepInput,bnWeightInput, bnRpeInput);
+                int bn1st = firstAttempt(bn1RM);
+                int bn2nd = secondAttempt(bn1RM);
+                int bn3rd = thirdAttempt(bn1RM);
 
-                Log.d("TAG", String.valueOf(sq1RM));
+                int dlRepInput = Integer.parseInt(dlRepID.getText().toString());
+                int dlWeightInput = Integer.parseInt(dlWeightID.getText().toString());
+                int dlRpeInput = Integer.parseInt(dlRpeID.getText().toString());
+                int dl1RM = oneRepMax(dlRepInput,dlWeightInput, dlRpeInput);
+                int dl1st = firstAttempt(dl1RM);
+                int dl2nd = secondAttempt(dl1RM);
+                int dl3rd = thirdAttempt(dl1RM);
 
 
                 Intent intent = new Intent(MainActivity.this, ResultActivity.class);
                 Bundle bundle = new Bundle();
-                bundle.putInt("Squat 1RM", sq1RM);
+                bundle.putInt("Squat1st", sq1st);
+                bundle.putInt("Squat2nd", sq2nd);
+                bundle.putInt("Squat3rd", sq3rd);
+
+                bundle.putInt("Bench1st", bn1st);
+                bundle.putInt("Bench2nd", bn2nd);
+                bundle.putInt("Bench3rd", bn3rd);
+
+                bundle.putInt("Deadlift1st", dl1st);
+                bundle.putInt("Deadlift2nd", dl2nd);
+                bundle.putInt("Deadlift3rd", dl3rd);
+
                 intent.putExtras(bundle);
                 startActivity(intent);
+
+                Log.d("TAG", String.valueOf(sq1RM));
 
             }
         });
 
     }
 
+    public int firstAttempt(int oneRepMax)
+    {
+        return (int)(oneRepMax * 0.90);
+    }
 
-    public int calculate1RM(int rep, int weight, int rpe)
+    public int secondAttempt(int oneRepMax)
+    {
+        return (int)(oneRepMax * 0.95);
+    }
+
+    public int thirdAttempt(int oneRepMax)
+    {
+        return oneRepMax;
+    }
+
+    public int oneRepMax(int rep, int weight, int rpe)
     {
         return (int)((weight*((10-(rpe+1))+rep)*0.03)+weight);
     }
--- app/src/main/java/com/jawstrength/powerliftingattemptselectioncalculator/ResultActivity.java
+++ app/src/main/java/com/jawstrength/powerliftingattemptselectioncalculator/ResultActivity.java
@@ -18,15 +18,21 @@ public class ResultActivity extends AppCompatActivity {
 
         TextView result = findViewById(R.id.txtResult);
 
+        int sq1Attempt = getIntent().getExtras().getInt("Squat1st");
+        int sq2Attempt = getIntent().getExtras().getInt("Squat2nd");
+        int sq3Attempt = getIntent().getExtras().getInt("Squat3rd");
+        result.setText(Integer.toString(sq1Attempt));
 
-        int sq1RMResult = getIntent().getExtras().getInt("Squat 1RM");
-        result.setText(Integer.toString(sq1RMResult));
+        int bn1Attempt = getIntent().getExtras().getInt("Bench1st");
+        int bn2Attempt = getIntent().getExtras().getInt("Bench2nd");
+        int bn3Attempt = getIntent().getExtras().getInt("Bench3rd");
+
+        int dl1Attempt = getIntent().getExtras().getInt("Deadlift1st");
+        int dl2Attempt = getIntent().getExtras().getInt("Deadlift2nd");
+        int dl3Attempt = getIntent().getExtras().getInt("Deadlift3rd");
 
 
-        Log.d("TAG", (String) result.getText());
 
-
-        //Toast.makeText(getApplicationContext(),"Hello toast", Toast.LENGTH_LONG).show();
 
 
 
