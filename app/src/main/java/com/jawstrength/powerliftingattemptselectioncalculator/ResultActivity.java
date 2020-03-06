7d70684fe591254c9655fbdb9d6b664c93fda80a
wajeht <zombyard@gmail.com>
2020-03-06 15:49:19 -0600

    - all of squat attempts work now. - fix all the ids to proper name!

--- app/src/main/java/com/jawstrength/powerliftingattemptselectioncalculator/ResultActivity.java
+++ app/src/main/java/com/jawstrength/powerliftingattemptselectioncalculator/ResultActivity.java
@@ -2,12 +2,8 @@ package com.jawstrength.powerliftingattemptselectioncalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -17,8 +13,16 @@ public class ResultActivity extends AppCompatActivity {
         setContentView(R.layout.activity_result);
 
         TextView sq1Low = findViewById(R.id.sq1Low);
-        TextView sq1Medium = findViewById(R.id.sq1Medium);
+        TextView sq1Normal = findViewById(R.id.sq1Normal);
         TextView sq1High = findViewById(R.id.sq1High);
+
+        TextView sq2Low = findViewById(R.id.sq2Low);
+        TextView sq2Normal = findViewById(R.id.sq2Normal);
+        TextView sq2High = findViewById(R.id.sq2High);
+
+        TextView sq3Low = findViewById(R.id.sq3Low);
+        TextView sq3Normal = findViewById(R.id.sq3Normal);
+        TextView sq3High = findViewById(R.id.sq3High);
 
 
         int sq1Attempt = getIntent().getExtras().getInt("Squat1st");
@@ -34,9 +38,17 @@ public class ResultActivity extends AppCompatActivity {
         int dl3Attempt = getIntent().getExtras().getInt("Deadlift3rd");
 
 
-        sq1Low.setText(String.valueOf((int)(sq1Attempt+(sq1Attempt * 0.01))));
-        sq1Medium.setText(String.valueOf(sq1Attempt));
+        sq1Low.setText(String.valueOf((int)(sq1Attempt-(sq1Attempt * 0.01))));
+        sq1Normal.setText(String.valueOf(sq1Attempt));
         sq1High.setText(String.valueOf((int)(sq1Attempt+(sq1Attempt * 0.02))));
+
+        sq2Low.setText(String.valueOf((int)(sq2Attempt-(sq2Attempt * 0.01))));
+        sq2Normal.setText(String.valueOf(sq2Attempt));
+        sq2High.setText(String.valueOf((int)(sq2Attempt+(sq2Attempt * 0.02))));
+
+        sq3Low.setText(String.valueOf((int)(sq3Attempt-(sq3Attempt * 0.01))));
+        sq3Normal.setText(String.valueOf(sq3Attempt));
+        sq3High.setText(String.valueOf((int)(sq3Attempt+(sq3Attempt * 0.02))));
 
 
 
