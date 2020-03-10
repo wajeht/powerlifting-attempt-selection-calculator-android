e9e45db5c32b19315e4253a5c82c762178d3ccaa
wajeht <zombyard@gmail.com>
2020-03-10 16:30:08 -0500

    - removed unused imports from mainActivity.java

--- app/src/main/java/com/jawstrength/powerliftingattemptselectioncalculator/MainActivity.java
+++ app/src/main/java/com/jawstrength/powerliftingattemptselectioncalculator/MainActivity.java
@@ -7,18 +7,13 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.text.Html;
-import android.text.method.LinkMovementMethod;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
 
